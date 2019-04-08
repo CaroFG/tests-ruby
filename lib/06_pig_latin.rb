5604060b5bded8868c286f532e8db006fb000240
CaroFG <carolina.ferreira131@gmail.com>
2019-04-08 23:16:53 +0200

    exo 06 un seul mot ok

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -2,27 +2,32 @@
 def translate(s)
 	if s.match(/^[aeiouy]/i)
 		return s + "ay" 
-	elsif s.match(/^[bcdfghjklmnpqrstvwxz][qu]/i)
-		fin3 = s.chars.take(3).join
-		return s.delete(fin3) + fin3 + "ay"
 	elsif s.match(/^[qu]/i)
 		fin2 = s.chars.take(2).join
 		return s.delete(fin2) + fin2 + "ay"
-	elsif s.match(/^[sch]/i)
-		fin2 = s.chars.take(3).join
-		return s.delete(fin2) + fin2 + "ay"
-	elsif s.match(/^[bcdfghjklmnpqrstvwxz][aeiouy]/i) 
-		fin = s.chars.take(1).join
-		return s.delete(fin) + fin + "ay"
-	elsif s.match(/[^aeiouy]{2}/i) 
-		fin2 = s.chars.take(2).join
-		return s.delete(fin2) + fin2 + "ay"
+	elsif s.match(/^[bcdfghjklmnpqrstvwxz][qu]/i)
+		fin3 = s.chars.take(3).join
+		return s.delete(fin3) + fin3 + "ay"
 	elsif s.match(/^[bcdfghjklmnpqrstvwxz]{3}/i)
 		fin3 = s.chars.take(3).join
 		return s.delete(fin3) + fin3 + "ay"
+	elsif s.match(/[^aeiouy]{2}[aeiouy]/i) 
+		fin2 = s.chars.take(2).join
+		return s.delete(fin2) + fin2 + "ay"
+	elsif s.match(/^[sch]/i)
+		fin3 = s.chars.take(3).join
+		return s.delete(fin3) + fin2 + "ay"
+	elsif s.match(/^[bcdfghjklmnpqrstvwxz][aeiouy]/i) 
+		fin = s.chars.take(1).join
+		return s.delete(fin) + fin + "ay"
+	
+	
 	
 	end
 end
+puts translate("cherry")
+puts translate("three")
+puts translate("quiet")
 
 #Nous avons tenté une approche différente mais ça n'a pas marché
 =begin
